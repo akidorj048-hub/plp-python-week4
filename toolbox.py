1fc2c0edb995170b846fcf5381c759be360f7623
JULIAS EROT AKIDOR <akidorj048@gmail.com>
2026-09-21 14:23:29 +0300

    Add files via upload

---
+++ toolbox.py
@@ -0,0 +1,22 @@
+# Question 2 - Your First Toolbox
+# Function 1 - double
+def double(number):
+    return(number*2)
+print(double(7))
+print(double(10))
+
+# Function 2 - is_pass
+def is_pass(score):
+    if score >= 50:
+        return "Pass"
+    return "Fail"
+print(is_pass(80))
+print(is_pass(20))
+
+
+# Function 3 - greet
+def greet(name, greeting):
+   return name + "," + greeting + "!"
+
+print(greet("Amina", " Hello"))
+print(greet("Brian", " Habari"))
---
+++ welcome.py
@@ -0,0 +1,12 @@
+# Step 2: Define the function
+def welcome(name):
+    return("Hello, " + name + "! Welcome to PLP.")
+
+# Step 3: Call the function with different names
+print(welcome("Amina"))
+print(welcome("Brian"))
+print(welcome("Fatuma"))
+
+
+
+
